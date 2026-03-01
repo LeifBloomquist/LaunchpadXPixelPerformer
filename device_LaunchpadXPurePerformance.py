89214b6757ab2ea990b9d28913ad1278d30d3970
Leif Bloomquist <github@leifbloomquist.net>
2026-03-01 18:40:09 -0500

    Added Display Zone navigation

--- device_LaunchpadXPurePerformance.py
+++ device_LaunchpadXPurePerformance.py
@@ -19,6 +19,12 @@ NOVATION_LOGO = 0x63
 RIGHT_ARROWS  = [0x59, 0x4F, 0x45, 0x3B, 0x31, 0x27, 0x1D, 0x13]
 TOP_BUTTONS   = [0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62]
 
+class Arrows:  # To avoid issues with the match down below
+    ARROW_UP      = TOP_BUTTONS[0]
+    ARROW_DOWN    = TOP_BUTTONS[1]
+    ARROW_LEFT    = TOP_BUTTONS[2]
+    ARROW_RIGHT   = TOP_BUTTONS[3]
+
 SESSION       = TOP_BUTTONS[4]
 CUSTOM        = TOP_BUTTONS[6]
 CAPTURE_MIDI  = TOP_BUTTONS[7]
@@ -33,6 +39,10 @@ CLIP_GRID = [
     list(range(0x15, 0x1D)),
     list(range(0x0B, 0x13)),
 ]
+
+GRID_SIZE=8
+grid_offset_x=0
+grid_offset_y=0
 
 # Timer for clearing all clips
 session_pressed = False
@@ -68,16 +78,38 @@ def ClearAllClips():
         playlist.triggerLiveClip(track+1,-1,midi.TLC_Fill)  # Stop the clips on this track
 
 
+def MoveGrid(delta_x, delta_y):
+    global grid_offset_x
+    global grid_offset_y
+
+    grid_offset_x += delta_x
+    grid_offset_y += delta_y
+    
+    if grid_offset_x < 0:
+        grid_offset_x = 0
+    
+    if grid_offset_y < 0:
+        grid_offset_y = 0
+    
+    if grid_offset_x > 50:
+        grid_offset_x = 50
+      
+    if grid_offset_y > 50:
+        grid_offset_y = 50
+    
+    playlist.liveDisplayZone(grid_offset_x, grid_offset_y + 1, grid_offset_x + GRID_SIZE, grid_offset_y + GRID_SIZE + 1, 200)
+
+
 def PaintTopRow():
     # Paint the top row of buttons     
-    PaintCell(TOP_BUTTONS[0], CurrentTheme().arrow_colors)    # Arrows
-    PaintCell(TOP_BUTTONS[1], CurrentTheme().arrow_colors)
-    PaintCell(TOP_BUTTONS[2], CurrentTheme().arrow_colors)
-    PaintCell(TOP_BUTTONS[3], CurrentTheme().arrow_colors)
-    PaintCell(SESSION,        CurrentTheme().session_default) # Session
-    PaintCell(TOP_BUTTONS[5], CurrentTheme().OFF)             # Note
-    PaintCell(CUSTOM,         CurrentTheme().logo_color)      # Custom
-    PaintCell(CAPTURE_MIDI,   CurrentTheme().stopped_color)   # Capture MIDI
+    PaintCell(Arrows.ARROW_UP,    CurrentTheme().arrow_colors)    # Arrows
+    PaintCell(Arrows.ARROW_DOWN,  CurrentTheme().arrow_colors)
+    PaintCell(Arrows.ARROW_LEFT,  CurrentTheme().arrow_colors)
+    PaintCell(Arrows.ARROW_RIGHT, CurrentTheme().arrow_colors)
+    PaintCell(SESSION,            CurrentTheme().session_default) # Session
+    PaintCell(TOP_BUTTONS[5],     CurrentTheme().OFF)             # Note
+    PaintCell(CUSTOM,             CurrentTheme().logo_color)      # Custom
+    PaintCell(CAPTURE_MIDI,       CurrentTheme().stopped_color)   # Capture MIDI
     
     # Paint Logo
     PaintCell(NOVATION_LOGO, CurrentTheme().logo_color)
@@ -91,7 +123,7 @@ def PaintAllButtons():
         
         for j, cell in enumerate(row):
             
-            block_status = playlist.getLiveBlockStatus(i+1, j, midi.LB_Status_Simple)   # Tracks indexed from 1
+            block_status = playlist.getLiveBlockStatus(i+1+grid_offset_y, j+grid_offset_x, midi.LB_Status_Simple)   # Tracks indexed from 1
             
             match block_status:
                 case 0:   # Empty
@@ -180,7 +212,7 @@ def OnMidiIn(event):
     for i, row in enumerate(CLIP_GRID):
         if event.data1 in row:
             block = row.index(event.data1)
-            playlist.triggerLiveClip(i+1, block, midi.TLC_MuteOthers | midi.TLC_Fill)
+            playlist.triggerLiveClip(i+1+grid_offset_y, block+grid_offset_x, midi.TLC_MuteOthers | midi.TLC_Fill)
 
     # Control playback with Capture MIDI button
     if event.data1 == CAPTURE_MIDI:
@@ -194,6 +226,23 @@ def OnMidiIn(event):
     if event.data1 == CUSTOM:
         NextTheme()
         PaintTopRow()
+
+    # Handle top arrow buttons for moving the grid 
+    match event.data1:
+        case Arrows.ARROW_UP:
+            MoveGrid(0,-1)
+                
+        case Arrows.ARROW_DOWN:
+            MoveGrid(0, 1)
+                
+        case Arrows.ARROW_LEFT:
+            MoveGrid(-1,0)
+            
+        case Arrows.ARROW_RIGHT:
+            MoveGrid(1, 0)
+        
+        case _:
+            pass
     
     # Refresh
     PaintAllButtons()
